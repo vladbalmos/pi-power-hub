13451b5120e8dd40304d94f888cdc000650687a2
Vlad Balmos <vlad.balmos@gmail.com>
2023-01-31 18:16:45 +0200

    WIP

--- src/net.py
+++ src/net.py
@@ -6,12 +6,5 @@ _device_registration_info = {}
 async def init(device_registration, main_msg_queue):
     # start timer to check if connected, cancel connect task if more than X seconds passed
     # if initial connection failed, retry until connected
-    pass
-        
-def set_registration_info(did, dname, dfeatures):
     global _device_registration_info
-    _device_registration_info = {
-        'id': did,
-        'name': dname,
-        'features': dfeatures
-    }
+    _device_registration_info = device_registration
